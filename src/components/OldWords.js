4a1582870ca3625397ccc697700d25092c00f83b
Daniel Villagrán <daniel.villagran@akkodis.com>
2024-12-29 13:07:53 +0100

    oldWords key

--- src/components/OldWords.js
+++ src/components/OldWords.js
@@ -1,4 +1,4 @@
 import {Button} from "@mui/material";
 export default function OldWords(props){
-    return <div className={'p-2 flex justify-start'}>{[...new Set(props.words)].map((el)=>{return <Button className={'border-white text-white flex'} onClick={()=>props.addWord(el)}>{el}</Button>})}</div>
+    return <div className={'p-2 flex justify-start'}>{[...new Set(props.words)].map((el,index)=>{return <Button key={index} className={'border-white text-white flex'} onClick={()=>props.addWord(el)}>{el}</Button>})}</div>
 }
